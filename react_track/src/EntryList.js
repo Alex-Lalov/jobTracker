e9b747292a85c81a434f63df424891fe44bd1866
Alex-Lalov <lalovalex@gmail.com>
2023-10-08 20:01:39 +0100

    Finished editing function in EntryList.js and added styles

--- react_track/src/EntryList.js
+++ react_track/src/EntryList.js
@@ -1,12 +1,15 @@
-export default function EntryList({ entries, handleToggle, handleDelete}){
+export default function EntryList({ entries, handleToggle, handleDelete, handleEdit, handleStatusUpdate}){
     return (
         <div id="cards">
             {entries.map((entry) => (
-            <div className="card">
+            <div className={`card ${entry.editing ? 'editing' : ''}`}>
                 <div className={`delete-icon ${entry.selected ? 'show' : 'hide'}`} onClick={ () => handleDelete(entry.id)}>
                     ✕
                 </div>
-                <div key={entry.id} className={`entry ${entry.selected ? 'blurred' : ''}`} onClick={() => handleToggle(entry.id)}>
+                <div className={`edit-icon ${entry.selected ? 'show' : 'hide'}`} onClick={ () => handleEdit(entry.id)}>
+                    <i class="fa-solid fa-pen"></i>
+                </div>
+                <div key={entry.id} className={`entry ${entry.selected ? 'blurred' : ''} ${entry.editing ? 'editing' : ''}`} onClick={() => handleToggle(entry.id)}>
                     <div className="entry-header">
                         <span className="company">{entry.job_title}@{entry.company}</span>
                     </div>
@@ -15,7 +18,17 @@ export default function EntryList({ entries, handleToggle, handleDelete}){
                     </div>
                     <div className="entry-footer">
                         <span className="date">{entry.date_string}</span>
-                        <span className="status">Status: {entry.status}</span>
+                        <span className="status">Status: {entry.editing ? (
+                            <input 
+                                className="status-update"
+                                type="text" 
+                                defaultValue={entry.status}
+                                onBlur={(e) => handleStatusUpdate(e, entry.id)}
+                            />
+                            ) : (
+                                entry.status
+                            )}
+                        </span>
                     </div>
                 </div>
             </div>
